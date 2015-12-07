7ed5dc9ab1c585e42ced0354e4860d183719251e
Ross Byrne <ross.byrne@outlook.ie>
2015-12-07 17:23:20 +0000

    Setting Up QuadGramMap.java
    QuadGramMap.java now has a constructor that uses FileParse to parse the 4gram.txt file into a hashmap.

--- src/ie/gmit/sw/QuadGramMap.java
+++ src/ie/gmit/sw/QuadGramMap.java
@@ -6,9 +6,25 @@ import java.util.concurrent.*;
 public class QuadGramMap {
 	
 	public static final int GRAM_SIZE = 4;
-	private Map<String, Double> map = new ConcurrentHashMap<String, Double>();
+	private static final String FILE_NAME = "4gram.txt";
+	private Map<String, Double> map; 
 	
 	// constructor that creates an instance of FileParser, reads the 4grams.txt file into the hashmap
+	public QuadGramMap(){
+		
+		FileParser parser = new FileParser();
+		
+		try {
+			
+			this.map = new ConcurrentHashMap<String, Double>(parser.parse(FILE_NAME));
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+			
+			System.out.println("Could Not Read QuadGram File!");
+		} // try catch
+		
+	} // QuadGramMap()
 	
-
 } // class
